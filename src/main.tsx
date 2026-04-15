e7e02b8998d2e525ad800dc65b11c3586c76be4e
J0S3PH12345 <josephtew9020@gmail.com>
2026-04-15 16:07:33 -0600

    preparation to deploy on github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./App.css";
 import { ListsProvider } from "./context/ListsContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ListsProvider>
         <App />
       </ListsProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
